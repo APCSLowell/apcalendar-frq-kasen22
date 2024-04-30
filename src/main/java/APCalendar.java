efb5aad50919778143e2a25d87bba57ba77bcf5a
kasen22 <143123847+kasen22@users.noreply.github.com>
2024-04-29 21:54:53 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -56,6 +56,7 @@ public class APCalendar
    */
   public static int dayOfWeek(int month, int day, int year)
   {
-    /* to be implemented in part (b) */
+    int additionalDays = dayOfYear(month, day, year) - 1;
+    return (firstDayOfYear(year) + additionalDays) % 7;
   }
 }
